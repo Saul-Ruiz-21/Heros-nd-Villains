8ab23191c715aaa7c0ce1c35ea5d1f0222ded813
Saul Ruiz <saulruiz730@gmail.com>
2022-10-10 12:35:04 -0500

    started project Heros and Villains

---
+++ Heros_and_Villains/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Heros_and_Villains project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Heros_and_Villains.settings')
+
+application = get_wsgi_application()
