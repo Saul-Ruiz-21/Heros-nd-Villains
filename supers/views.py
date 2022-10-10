2b177b84a36ab6d41a6ce82cb622b3353f936cc3
Saul Ruiz <saulruiz730@gmail.com>
2022-10-10 12:59:23 -0500

    added urls file to supers folder and created model for super as well

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from urllib import response
+from rest_framework import status
+from rest_framework import api_view
+from rest_framework import response
+from .models import supers
