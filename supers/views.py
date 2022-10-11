a529f5102133cd9138f3551d129cd8d0572a5d31
Saul Ruiz <saulruiz730@gmail.com>
2022-10-11 09:35:50 -0500

    finished supers functions and added url paths for supers

--- supers/views.py
+++ supers/views.py
@@ -3,7 +3,6 @@ from urllib import response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import supers
 from .models import Super
 from .serializers import SuperSerializer
 from supers import serializers
@@ -22,3 +21,17 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def supers_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
